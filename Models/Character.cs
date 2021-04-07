17153cc5b005c4dac7b9fe922d083daec2738d3d
William Lee <william.williamlee@gmail.com>
2021-04-07 18:11:12 +0800

    init

---
+++ Models/Character.cs
@@ -0,0 +1,13 @@
+namespace dotnet_rpg_1.Models
+{
+  public class Character
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = "William";
+    public int HitPoints { get; set; } = 10;
+
+
+
+
+  }
+}
